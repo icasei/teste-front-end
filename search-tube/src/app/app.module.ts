1a8e5c8cabe64d7529c34c708cf6aec32b7a2cf5
Andre Souza <andre.souza@solucioneonline.com.br>
2019-09-27 16:00:32 -0300

    adding angular material

--- search-tube/src/app/app.module.ts
+++ search-tube/src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
