3e2601cc818e4b1720b7b6832e36b25d6a6da286
Leonardo Falotico <leonardo.falotico@edev.com.br>
2019-09-29 11:27:22 -0300

    Criação de Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { SearchComponent } from './main/search/search.component';
+import { DetailsComponent } from './main/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SearchComponent,
+    DetailsComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
