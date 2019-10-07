d14a12d25248225195f09c3b6e126ba534cc3a2e
Gabriel Furquim <gabriel.furquim@outlook.com>
2019-10-06 21:54:19 -0300

    done card feature.

---
+++ src/Components/Card/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import { Link } from 'react-router-dom'
+
+import S from './style'
+
+
+const Card = ({
+  picture,
+  title,
+  subtitle,
+  description,
+  link
+}) => {
+  return (
+    <>
+      <S.Body>
+        <S.Picture src={picture} />
+        <S.Title>{title}</S.Title>
+        <S.Subtitle> {subtitle}</S.Subtitle>
+        <S.Description>{description}</S.Description>
+        <S.Footer>
+          <Link to={link}>
+            <Button variant='contained'> detalhes do vídeo</Button>
+          </Link>
+        </S.Footer>
+      </S.Body>
+    </>
+  )
+}
+
+export default Card
