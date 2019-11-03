2a5c9dc0457f41b99475b01d2737282b70d7c281
guihenrry <guilhermehenry60@gmail.com>
2019-11-03 12:42:28 -0300

    add search input

---
+++ src/components/VideoSearch.vue
@@ -0,0 +1,73 @@
+<template>
+  <form class="search" @submit.prevent>
+    <input
+      v-model="search"
+      class="search__input"
+      type="text"
+      placeholder="Pesquisar"
+      @keyup.enter="searchVideos"
+    />
+    <input class="search__button" type="button" value="Buscar" @click="searchVideos" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "VideosSearch",
+  data() {
+    return {
+      search: ""
+    };
+  },
+  methods: {
+    searchVideos() {
+      if (this.$route.query.search_query !== this.search) {
+        this.$router.push({ query: { search_query: this.search } });
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.search {
+  display: grid;
+}
+
+.search__input {
+  grid-area: 1/1;
+  background: linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 0.09),
+      rgba(255, 255, 255, 0.09)
+    ),
+    #121212;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2), 0px 1px 10px rgba(0, 0, 0, 0.12),
+    0px 4px 5px rgba(0, 0, 0, 0.14);
+  border: none;
+  border-radius: 4px;
+  padding: 15px 50px 15px 15px;
+  width: 100%;
+  font-size: 1rem;
+  color: rgba(255, 255, 255, 0.9);
+}
+
+.search__input:hover {
+  background: rgba(255, 255, 255, 0.04);
+}
+
+.search__input:focus {
+  background: rgba(255, 255, 255, 0.12);
+}
+
+.search__button {
+  grid-area: 1/1;
+  width: 50px;
+  justify-self: end;
+  text-indent: -100px;
+  border: none;
+  background: transparent url("../assets/icon-search.svg") no-repeat center
+    center;
+  cursor: pointer;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="seach seach--middle-screen">
+    <VideoSearch />
+  </div>
+</template>
+
+<script>
+import VideoSearch from "@/components/VideoSearch.vue";
+
+export default {
+  name: "Home",
+  components: {
+    VideoSearch
+  }
+};
+</script>
+
+<style scoped>
+.seach {
+  position: absolute;
+  top: 60px;
+  left: 50%;
+  transform: translate3d(-50%, 0, 0);
+  width: 642px;
+  max-width: 100%;
+  padding: 0 15px;
+  transition: top 0.3s;
+}
+
+.seach--middle-screen {
+  top: 50%;
+  transform: translate3d(-50%, -50%, 0);
+}
+</style>
