f877c781c1a2b19020e52033e8d15347e5056f30
bruno pessoa <brunopessoa.melo@gmail.com>
2020-11-03 17:42:09 -0300

    instalando o material UI e criado e testando o Header.

---
+++ youtube-clone/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Eu sou o componente Header</h1>
+        </div>
+    );
+}
+
+export default Header
---
+++ youtube-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD_XtZ8hh3n0QxUd6u0WkQyKmCHRHKiFmc",
+    authDomain: "menumobile-d3d07.firebaseapp.com",
+    databaseURL: "https://menumobile-d3d07.firebaseio.com",
+    projectId: "menumobile-d3d07",
+    storageBucket: "menumobile-d3d07.appspot.com",
+    messagingSenderId: "732745517706",
+    appId: "1:732745517706:web:fae83394c672b49bd63b73"
+  };
