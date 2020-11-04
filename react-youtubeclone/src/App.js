67b9c0e4d767c36f03399f475bbbaf7e676bbedf
bruno pessoa <brunopessoa.melo@gmail.com>
2020-11-04 18:11:10 -0300

    erro de componente SearchBar não está sendo importado no App.js

--- react-youtubeclone/src/App.js
+++ react-youtubeclone/src/App.js
@@ -9,6 +9,34 @@ export default () => {
   const [selectVideo, setSelectedVideo] = useState(null);
 
   return(
-    <Grid style
+    <Grid style={{ justifyContent:"center"}} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onSubmit={handleSumit} />
+          </Grid>
+          <Grid item xs={8}>
+            <VideoDetail video={selectVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList videos={video} onVideoSelect={setSelectedVideo} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
+
+  async function handleSubmit(searchTerm){
+    const {data: {items:videos}} = await youtube.get("search", {
+      params:{
+        part: "snippet",
+        maxResults: 5,
+        key: process.env.React_APP_API_KEY,
+        q: searchTerm,
+      }
+    });
+
+    setVideos(videos);
+    setSelectedVideo(videos[0]);
+  }
 }
