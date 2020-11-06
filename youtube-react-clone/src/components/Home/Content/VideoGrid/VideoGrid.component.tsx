1eddd7d965802f90b96a50e65f5a90756adda384
bruno pessoa <brunopessoa.melo@gmail.com>
2020-11-06 17:02:22 -0300

    instalando o axios, material-ui, testando a lógica do VideoPreview.

---
+++ youtube-react-clone/src/components/Home/Content/VideoGrid/VideoGrid.component.tsx
@@ -0,0 +1,8 @@
+
+import React from 'react'
+import { VideoPreview } from './VideoPreview/VideoPreview.component';
+
+export default function VideoGrid.component() {
+    return <VideoPreview />
+}
+
---
+++ youtube-react-clone/src/components/Home/Content/VideoGrid/VideoPreview/VideoPreview.component.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const VideoPreview = () => {
+    return <div>Video PReview</div>
+}
