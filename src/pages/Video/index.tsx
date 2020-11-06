b1e3d2666581c8dd22d6d889a9b4d02ed95f7be3
DougGoncalves <doug.goncalvesf@gmail.com>
2020-11-06 15:31:55 -0300

    add:Estruturação do Projeto e Componentização

---
+++ src/pages/Video/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Video() {
+  return (
+    <div>
+      <h1>VIDEO</h1>
+    </div>
+  )
+}
+
+export default Video
---
+++ src/utils/api.ts
@@ -0,0 +1,5 @@
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search'
+const YOUTUBE_VIDEO_URL = 'https://www.googleapis.com/youtube/v3/videos'
+const API_KEY = 'AIzaSyC1-qtw-x8ocITOYzSElz2LLAZlr7hSsE0'
+
+export { YOUTUBE_SEARCH_URL, YOUTUBE_VIDEO_URL, API_KEY}
